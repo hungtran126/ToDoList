227b405710e9d0f790d1e2706b55a681ab5515f2
hungtran126 <65010252+hungtran126@users.noreply.github.com>
2020-08-04 21:37:46 +0700

    Update TodoList.js

--- src/component/Body/TodoList.js
+++ src/component/Body/TodoList.js
@@ -12,123 +12,120 @@ class TodoList extends Component {
             newItems: '',
             id: '',
             title: '',
-            TodoItems: [
-                { id: 1, title: 'I do first', isComplete: false, showEditForm: false},
-                { id: 2, title: 'huhu', isComplete: false, showEditForm: false},
-                { id: 3, title: 'Hehe', isComplete: false, showEditForm: false}
-            ]
+            TodoItems: []
         }
-        
-        this.createItems = this.createItems.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.checkAl = this.checkAl.bind(this);
+        this.createItems = this.createItems.bind(this)
+        this.onChange = this.onChange.bind(this)
+        this.checkAl = this.checkAl.bind(this)
     }
     handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
+        const target = event.target
+        const value = target.value
+        const name = target.name
         this.setState({
           [name]: value
-        });
-
-    };
+        })
+        console.log(target)
+    }
     handleEditSubmit = (event) => {
-        event.preventDefault();
+        event.preventDefault()
     
         let key = this.state.id;
         this.setState(prevState => ({
             TodoItems: prevState.TodoItems.map(
-                elm => elm.id === key ? {
-                ...elm,
-                title: this.state.title,
-                isComplete: false
-                }: elm
+                item => item.id === key ? { 
+                    ...item,
+                    title: this.state.title,
+                    isComplete: false
+                }: item
             ),
             modalIsOpen: false
         
         }))
         console.log(key)
-    };
+    }
     onItemClicked(item) {
-        return (event) => {
-          const isComplete = item.isComplete;
-          const { TodoItems } = this.state;
-          const index = TodoItems.indexOf(item);
+        return () => {
+          const isComplete = item.isComplete
+          const { TodoItems } = this.state
+          const index = TodoItems.indexOf(item)
           this.setState({
             TodoItems: [...TodoItems.slice(0, index),{...item, isComplete: !isComplete}, ...TodoItems.slice(index + 1)]
-          });
-        };
+          })
+          console.log(index)
+        }
+        
     }
     checkAl(event){
-    let TodoItems = this.state.TodoItems;
-    TodoItems.forEach(TodoItem => {
-        if(TodoItem.value === event.target.value){
-        TodoItem.isComplete = event.target.checked
-        }
-    });
-    console.log(event.target.value);
-    this.setState({isComplete: TodoItems});
+        let TodoItems = this.state.TodoItems
+        TodoItems.forEach(TodoItem => {
+            if(TodoItem.value === event.target.value){
+            TodoItem.isComplete = event.target.checked
+            }
+        })
+        console.log(event.target.value)
+        this.setState({isComplete: TodoItems})
     }
     createItems(event) {
-    if(event.keyCode === 13){
-        let text = event.target.value;
-        if(!text) {
-        return;
+        if(event.keyCode === 13){
+            let text = event.target.value
+            if(!text) {
+                return
+            }
+            text = text.trim()
+            if(!text) {
+                return 
+            }
+            const {TodoItems} = this.state
+            let idd = 0
+            for(idd = 0; idd<TodoItems.length; idd++){}
+            this.setState({
+                newItems: '',
+                TodoItems: [
+                    ...this.state.TodoItems,
+                    {id: idd, title: text, isComplete: false}
+                ]
+            })   
         }
-        text = text.trim();
-        if(!text) {
-        return;
-        }
-        this.setState({
-        newItems: '',
-        TodoItems: [
-            ...this.state.TodoItems,
-            { title: text, isComplete: false}
-        ]
-        });   
-    }
     }
     onChange(event) {
-    this.setState({
-        newItems: event.target.value
-    });
+        this.setState({
+            newItems: event.target.value
+        })
     }
     deleteTask  = (id) =>{
-        const { TodoItems } = this.state;
-        // const item = TodoItems.filter(TodoItems.id===id)
-        // const index = TodoItems.indexOf(id);
+        const { TodoItems } = this.state
         const index = TodoItems.findIndex(item=>item.id===id)
         if(index !== -1){
         let TodoItem = TodoItems.splice(index, 1)
         this.setState({title: TodoItem})
         }
         
-        // this.setState({title: TodoItems});
-        console.log(id);
+        // this.setState({title: TodoItems})
+        console.log(index)
     }
     componentWillMount() {
-        Modal.setAppElement('body');
-    };
+        Modal.setAppElement('body')
+    }
     openModal = (item) => {
-        // const {TodoItems} = this.state
         this.setState({
             modalIsOpen: true,
             id: item.id,
             title: item.title
-        });
-    };
+        })
+    }
 
     closeModal = () => {
         this.setState({
             modalIsOpen: false
-        });
-    };
+        })
+    }
     render() {
-        const { TodoItems, newItems } = this.state;
+        const { TodoItems, newItems } = this.state
         return (
             <div className="boxwhite">
                 <div className="head">
-                    <img onClick={this.checkAl} src={checkAll} width={32} height={32} />
+                    {/* <img onClick={this.checkAl} src={checkAll} width={32} height={32} alt="img" /> */}
                     <input type="text" placeholder="Add a new item" value={newItems} onChange={this.onChange} onKeyUp={this.createItems} />
                 </div>
                 {TodoItems.length > 0 && TodoItems.map((item) => <TodoItem key={item.id}
@@ -140,13 +137,14 @@ class TodoList extends Component {
                 />)}
                 {TodoItems.length === 0 && <h5>Nothing here</h5>}
                 <Modal className="modals"
-                    isOpen={this.state.modalIsOpen}
-                    onRequestClose={this.closeModal}>
+                        isOpen={this.state.modalIsOpen}
+                        onRequestClose={this.closeModal}>
+                    <h1 className="text-center">Edit</h1>
                     <form onSubmit={this.handleEditSubmit}>
                         <input type="text" name="title" value={this.state.title} onChange={this.handleInputChange} /> 
-                        <button type="submit"><img src={checkAll} width={32} height={32}/></button>
+                        <button type="submit"><img src={checkAll} width={32} height={32} alt="img1" /></button>
                     </form>
-                    <button onClick={this.closeModal}>Close</button>
+                    <button onClick={this.closeModal} className="">Close</button>
                 </Modal>
             </div>
         )
