e66f2a503e690908751c13b730386691068c5322
hungtran126 <65010252+hungtran126@users.noreply.github.com>
2020-08-04 21:37:36 +0700

    Update App.js

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import TodoList from '../Body/TodoList';
-import EditTask from '../Edit/EditTask';
+// import EditTask from '../Edit/EditTask';
 class App extends Component {
   render() {
     return (
@@ -9,7 +9,6 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={TodoList} />
-          <Route path="/edit/:id" component={EditTask} />
         </Switch>
       </div>
       </Router>
